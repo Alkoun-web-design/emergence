e041332175a960d61b6911b5b6e1b064efc456c6
Hassam Sajid <hassam.sajid.dvlpr@gmail.com>
2026-02-21 06:08:39 +0500

    added border and shadow to cards

--- components/ModuleCard.tsx
+++ components/ModuleCard.tsx
@@ -1,7 +1,7 @@
-export default function ModuleCard({ image, name, description, slug }: {image:string; name: string; description: string; slug: string;}) {
+export default function ModuleCard({ image, name, description, price, discount, discountPrice, slug }: {image:string; name: string; description: string; price: string, discount: boolean, discountPrice: string, slug: string;}) {
   return (
     // <div className="col-span-3 p-4 border-2 border-gray-900 rounded-md backdrop-blur-2xl bg-gray-100">
-    <div className="col-span-4 p-4 w-full mx-1 rounded-md backdrop-blur-2xl bg-gray-100 shadow-gray-900/30 shadow-xl mb-8 hover:shadow-2xl hover:-translate-y-1.5 duration-500 transition-all">
+    <div className="col-span-4 p-4 w-full mx-1 rounded-md backdrop-blur-2xl bg-gray-100 shadow-gray-900/30 border border-[#C1CAA0] mb-8 hover:shadow-lg hover:-translate-y-1.5 duration-500 transition-all">
         <div className="h-48 w-full my-2">
             <img className="object-cover" src={image} alt={name} loading="lazy"/>
         </div>
@@ -12,8 +12,13 @@ export default function ModuleCard({ image, name, description, slug }: {image:st
         <div>
             <img className="h-20 w-20 rounded-full" src="/" alt="speaker"/>
         </div>
-        <div className="my-2">
-            <button className="py-2 px-4 rounded-lg bg-lime-800 hover:cursor-pointer text-gray-100">Enroll Now</button>
+        <div className="flex my-2">
+            <button className="py-2 px-4 rounded-lg bg-[#C1CAA0] hover:cursor-pointer text-gray-100">Enroll Now</button>
+            { discount ? (
+                <><span className="mx-2">{price}</span><span className="mx-2">{discountPrice}</span></>
+            ) : (
+            <span className="mx-2">{price}</span>
+            )}
         </div>
     </div>
   )
