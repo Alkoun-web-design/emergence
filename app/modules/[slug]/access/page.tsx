7420751b8d8aa23eaefa8b1b80972585ea130381
alkoun-web-design <hassam.sajid.dvlpr@gmail.com>
2026-03-19 02:25:38 +0500

    added clerk middleware config and protected routes

---
+++ app/modules/[slug]/access/page.tsx
@@ -0,0 +1,5 @@
+export default function Page(){
+    return (
+        <h1>THis is the restricted page access for only paid and registered users.</h1>
+    )
+}
---
+++ app/modules/[slug]/page.tsx
@@ -0,0 +1,9 @@
+export default function Page(){
+    return (
+        <>
+            <h1>Module Details</h1>
+            <h2>Payment Link for Module if Unpaid</h2>
+            <h2>Else the module access button if the user has paid.</h2>
+        </>
+    )
+}
