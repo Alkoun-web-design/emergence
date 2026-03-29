7b8b240054ee6189172d7b2a84c8240a2a6badd8
alkoun-web-design <hassam.sajid.dvlpr@gmail.com>
2026-03-29 05:14:08 +0500

    added next cookie plugion to better auth

--- auth.ts
+++ auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { nextCookies } from 'better-auth/next-js'
-import Database from "better-sqlite3";
+import Database from "better-sqlite3"; 
 
 export const auth = betterAuth({
   database: new Database("./sqlite.db"),
